a96dd84a411677f71f21b48fc9a99ce9631987ba
Horikawa <m21137@g.metro-cit.ac.jp>
2025-09-25 09:29:52 +0900

    add 5.3.1

--- adapter/src/repository/book.rs
+++ adapter/src/repository/book.rs
@@ -1,10 +1,128 @@
+// use crate::database::model::book::BookRow;
+// use anyhow::Result;
+// use async_trait::async_trait;
+// use derive_new::new;
+// use kernel::model::book::{event::CreateBook, Book};
+// use kernel::repository::book::BookRepository;
+// use uuid::Uuid;
+// use shared::error::{AppError, AppResult};
+
+// use crate::database::ConnectionPool;
+
+// #[derive(new)]
+// pub struct BookRepositoryImpl {
+//     db: ConnectionPool,
+// }
+
+// #[async_trait]
+// impl BookRepository for BookRepositoryImpl {
+//     async fn create(&self, event: CreateBook) -> AppResult<()> {
+//         sqlx::query!(
+//             r#"
+//                 INSERT INTO books (title, author, isbn, description)
+//                 VALUES($1, $2, $3, $4)
+//             "#,
+//             event.title,
+//             event.author,
+//             event.isbn,
+//             event.description
+//         )
+//         .execute(self.db.inner_ref())
+//         .await
+//         .map_err(AppError::SpecificOperationError)?;
+
+//         Ok(())
+//     }
+
+//     async fn find_all(&self) -> Result<Vec<Book>> {
+//         let rows: Vec<BookRow> = sqlx::query_as!(
+//             BookRow,
+//             r#"
+//                 SELECT
+//                     book_id,
+//                     title,
+//                     author,
+//                     isbn,
+//                     description
+//                 FROM books
+//                 ORDER BY created_at DESC
+//             "#
+//         )
+//         .fetch_all(self.db.inner_ref())
+//         .await?;
+
+//         Ok(rows.into_iter().map(Book::from).collect())
+//     }
+
+//     async fn find_by_id(&self, book_id: Uuid) -> Result<Option<Book>> {
+//         let row: Option<BookRow> = sqlx::query_as!(
+//             BookRow,
+//             r#"
+//                 SELECT
+//                     book_id,
+//                     title,
+//                     author,
+//                     isbn,
+//                     description
+//                 FROM books
+//                 WHERE book_id = $1
+//             "#,
+//             book_id as _
+//         )
+//         .fetch_optional(self.db.inner_ref())
+//         .await?;
+
+//         Ok(row.map(Book::from))
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[sqlx::test]
+//     async fn test_register_book(pool: sqlx::PgPool) -> anyhow::Result<()> {
+//         let repo = BookRepositoryImpl::new(ConnectionPool::new(pool));
+
+//         let book = CreateBook {
+//             title: "Test Title".into(),
+//             author: "Test Author".into(),
+//             isbn: "Test ISBN".into(),
+//             description: "Test Description".into(),
+//         };
+
+//         repo.create(book).await?;
+
+//         let res = repo.find_all().await?;
+//         assert_eq!(res.len(), 1);
+
+//         let book_id = res[0].id;
+//         let res = repo.find_by_id(book_id).await?;
+//         assert!(res.is_some());
+
+//         let Book {
+//             id,
+//             title,
+//             author,
+//             isbn,
+//             description,
+//         } = res.unwrap();
+//         assert_eq!(id, book_id);
+//         assert_eq!(title, "Test Title");
+//         assert_eq!(author, "Test Author");
+//         assert_eq!(isbn, "Test ISBN");
+//         assert_eq!(description, "Test Description");
+
+//         Ok(())
+//     }
+// }
 use crate::database::model::book::BookRow;
-use anyhow::Result;
 use async_trait::async_trait;
 use derive_new::new;
 use kernel::model::book::{event::CreateBook, Book};
+use kernel::model::id::BookId;
 use kernel::repository::book::BookRepository;
-use uuid::Uuid;
+use shared::error::{AppError, AppResult};
 
 use crate::database::ConnectionPool;
 
@@ -15,7 +133,7 @@ pub struct BookRepositoryImpl {
 
 #[async_trait]
 impl BookRepository for BookRepositoryImpl {
-    async fn create(&self, event: CreateBook) -> Result<()> {
+    async fn create(&self, event: CreateBook) -> AppResult<()> {
         sqlx::query!(
             r#"
                 INSERT INTO books (title, author, isbn, description)
@@ -27,12 +145,13 @@ impl BookRepository for BookRepositoryImpl {
             event.description
         )
         .execute(self.db.inner_ref())
-        .await?;
+        .await
+        .map_err(AppError::SpecificOperationError)?;
 
         Ok(())
     }
 
-    async fn find_all(&self) -> Result<Vec<Book>> {
+    async fn find_all(&self) -> AppResult<Vec<Book>> {
         let rows: Vec<BookRow> = sqlx::query_as!(
             BookRow,
             r#"
@@ -47,12 +166,13 @@ impl BookRepository for BookRepositoryImpl {
             "#
         )
         .fetch_all(self.db.inner_ref())
-        .await?;
+        .await
+        .map_err(AppError::SpecificOperationError)?;
 
         Ok(rows.into_iter().map(Book::from).collect())
     }
 
-    async fn find_by_id(&self, book_id: Uuid) -> Result<Option<Book>> {
+    async fn find_by_id(&self, book_id: BookId) -> AppResult<Option<Book>> {
         let row: Option<BookRow> = sqlx::query_as!(
             BookRow,
             r#"
@@ -65,10 +185,11 @@ impl BookRepository for BookRepositoryImpl {
                 FROM books
                 WHERE book_id = $1
             "#,
-            book_id
+            book_id as _
         )
         .fetch_optional(self.db.inner_ref())
-        .await?;
+        .await
+        .map_err(AppError::SpecificOperationError)?;
 
         Ok(row.map(Book::from))
     }
